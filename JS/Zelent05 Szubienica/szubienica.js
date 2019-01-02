48d604be939bc5a1b2dde1b98b9156bbcf3d0994
Bandyta <lukasz.grymulski@gmail.com>
2019-01-02 04:21:03 +0100

    create letters with onclick

--- JS/Zelent05 Szubienica/szubienica.js
+++ JS/Zelent05 Szubienica/szubienica.js
@@ -15,8 +15,33 @@ function wypisz_haslo(){
     document.getElementById("plansza").innerHTML = haslo1;
 }
 
-window.onload = wypisz_haslo;
+window.onload = start;
+
+var litery = [
+    'A', 'Ą', 'B', 'C', 'Ć',
+    'D','E', 'Ę', 'F', 'G', 
+    'H','I', 'J', 'K', 'L', 
+    'Ł','M','N','Ń','O',
+    'Ó','P','Q','R','S',
+    'Ś','T','U','V','W',
+    'X','Y','Z','Ź', 'Ż'
+];
 
 function start(){
-    
+    var tresc_diva = "";
+
+    for (i=0; i<35; i++){
+        var element = "lit" + i;
+        tresc_diva += '<div class="litera" id="' + element + '" + onclick="sprawdz(' + i + ')">' + litery[i] + '</div>';
+        if (i%7 == 6) {
+            tresc_diva += '<div style="clear:both;"></div>';
+        }
+    }
+
+    document.getElementById("alfabet").innerHTML = tresc_diva;
+    wypisz_haslo();
+}
+
+function sprawdz(nr){
+    alert(nr);
 }
